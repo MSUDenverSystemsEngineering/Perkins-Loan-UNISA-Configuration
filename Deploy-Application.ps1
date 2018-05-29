c86c970618b9a504c872806f5d649f13db3f3e0e
Steve Patterson <spatte30@msudenver.edu>
2018-05-29 09:10:51 -0600

    initial commit

---
+++ Deploy-Application.ps1
@@ -0,0 +1,343 @@
+﻿<#
+.SYNOPSIS
+	This script performs the installation or uninstallation of an application(s).
+	# LICENSE #
+	PowerShell App Deployment Toolkit - Provides a set of functions to perform common application deployment tasks on Windows.
+	Copyright (C) 2017 - Sean Lillis, Dan Cunningham, Muhammad Mashwani, Aman Motazedian.
+	This program is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the Free Software Foundation, either version 3 of the License, or any later version. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+	You should have received a copy of the GNU Lesser General Public License along with this program. If not, see <http://www.gnu.org/licenses/>.
+.DESCRIPTION
+	The script is provided as a template to perform an install or uninstall of an application(s).
+	The script either performs an "Install" deployment type or an "Uninstall" deployment type.
+	The install deployment type is broken down into 3 main sections/phases: Pre-Install, Install, and Post-Install.
+	The script dot-sources the AppDeployToolkitMain.ps1 script which contains the logic and functions required to install or uninstall an application.
+.PARAMETER DeploymentType
+	The type of deployment to perform. Default is: Install.
+.PARAMETER DeployMode
+	Specifies whether the installation should be run in Interactive, Silent, or NonInteractive mode. Default is: Interactive. Options: Interactive = Shows dialogs, Silent = No dialogs, NonInteractive = Very silent, i.e. no blocking apps. NonInteractive mode is automatically set if it is detected that the process is not user interactive.
+.PARAMETER AllowRebootPassThru
+	Allows the 3010 return code (requires restart) to be passed back to the parent process (e.g. SCCM) if detected from an installation. If 3010 is passed back to SCCM, a reboot prompt will be triggered.
+.PARAMETER TerminalServerMode
+	Changes to "user install mode" and back to "user execute mode" for installing/uninstalling applications for Remote Destkop Session Hosts/Citrix servers.
+.PARAMETER DisableLogging
+	Disables logging to file for the script. Default is: $false.
+.EXAMPLE
+    powershell.exe -Command "& { & '.\Deploy-Application.ps1' -DeployMode 'Silent'; Exit $LastExitCode }"
+.EXAMPLE
+    powershell.exe -Command "& { & '.\Deploy-Application.ps1' -AllowRebootPassThru; Exit $LastExitCode }"
+.EXAMPLE
+    powershell.exe -Command "& { & '.\Deploy-Application.ps1' -DeploymentType 'Uninstall'; Exit $LastExitCode }"
+.EXAMPLE
+    Deploy-Application.exe -DeploymentType "Install" -DeployMode "Silent"
+.NOTES
+	Toolkit Exit Code Ranges:
+	60000 - 68999: Reserved for built-in exit codes in Deploy-Application.ps1, Deploy-Application.exe, and AppDeployToolkitMain.ps1
+	69000 - 69999: Recommended for user customized exit codes in Deploy-Application.ps1
+	70000 - 79999: Recommended for user customized exit codes in AppDeployToolkitExtensions.ps1
+.LINK
+	http://psappdeploytoolkit.com
+#>
+[CmdletBinding()]
+Param (
+	[Parameter(Mandatory=$false)]
+	[ValidateSet('Install','Uninstall')]
+	[string]$DeploymentType = 'Install',
+	[Parameter(Mandatory=$false)]
+	[ValidateSet('Interactive','Silent','NonInteractive')]
+	[string]$DeployMode = 'Interactive',
+	[Parameter(Mandatory=$false)]
+	[switch]$AllowRebootPassThru = $false,
+	[Parameter(Mandatory=$false)]
+	[switch]$TerminalServerMode = $false,
+	[Parameter(Mandatory=$false)]
+	[switch]$DisableLogging = $false
+)
+
+Try {
+	## Set the script execution policy for this process
+	Try { Set-ExecutionPolicy -ExecutionPolicy 'ByPass' -Scope 'Process' -Force -ErrorAction 'Stop' } Catch { Write-Error "Failed to set the execution policy to Bypass for this process." }
+
+	##*===============================================
+	##* VARIABLE DECLARATION
+	##*===============================================
+	## Variables: Application
+	[string]$appVendor = 'MSU Denver'
+	[string]$appName = 'Perkins Configuration'
+	[string]$appVersion = '1.0'
+	[string]$appArch = ''
+	[string]$appLang = 'EN'
+	[string]$appRevision = '01'
+	[string]$appScriptVersion = '1.0.0.0'
+	[string]$appScriptDate = '05/29/2018'
+	[string]$appScriptAuthor = 'Steve Patterson'
+	##*===============================================
+	## Variables: Install Titles (Only set here to override defaults set by the toolkit)
+	[string]$installName = ''
+	[string]$installTitle = ''
+
+	##* Do not modify section below
+	#region DoNotModify
+
+	## Variables: Exit Code
+	[int32]$mainExitCode = 0
+
+	## Variables: Script
+	[string]$deployAppScriptFriendlyName = 'Deploy Application'
+	[version]$deployAppScriptVersion = [version]'3.7.0'
+	[string]$deployAppScriptDate = '02/13/2018'
+	[hashtable]$deployAppScriptParameters = $psBoundParameters
+
+	## Variables: Environment
+	If (Test-Path -LiteralPath 'variable:HostInvocation') { $InvocationInfo = $HostInvocation } Else { $InvocationInfo = $MyInvocation }
+	[string]$scriptDirectory = Split-Path -Path $InvocationInfo.MyCommand.Definition -Parent
+
+	## Dot source the required App Deploy Toolkit Functions
+	Try {
+		[string]$moduleAppDeployToolkitMain = "$scriptDirectory\AppDeployToolkit\AppDeployToolkitMain.ps1"
+		If (-not (Test-Path -LiteralPath $moduleAppDeployToolkitMain -PathType 'Leaf')) { Throw "Module does not exist at the specified location [$moduleAppDeployToolkitMain]." }
+		If ($DisableLogging) { . $moduleAppDeployToolkitMain -DisableLogging } Else { . $moduleAppDeployToolkitMain }
+	}
+	Catch {
+		If ($mainExitCode -eq 0){ [int32]$mainExitCode = 60008 }
+		Write-Error -Message "Module [$moduleAppDeployToolkitMain] failed to load: `n$($_.Exception.Message)`n `n$($_.InvocationInfo.PositionMessage)" -ErrorAction 'Continue'
+		## Exit the script, returning the exit code to SCCM
+		If (Test-Path -LiteralPath 'variable:HostInvocation') { $script:ExitCode = $mainExitCode; Exit } Else { Exit $mainExitCode }
+	}
+
+	#endregion
+	##* Do not modify section above
+	##*===============================================
+	##* END VARIABLE DECLARATION
+	##*===============================================
+
+	If ($deploymentType -ine 'Uninstall') {
+		##*===============================================
+		##* PRE-INSTALLATION
+		##*===============================================
+		[string]$installPhase = 'Pre-Installation'
+
+		## Show Welcome Message, close Internet Explorer if needed, verify there is enough disk space to complete the install, and persist the prompt
+		Show-InstallationWelcome -CloseApps 'iexplore' -CheckDiskSpace -PersistPrompt
+
+		## Show Progress Message (with the default message)
+		Show-InstallationProgress
+
+		## <Perform Pre-Installation tasks here>
+
+
+		##*===============================================
+		##* INSTALLATION
+		##*===============================================
+		[string]$installPhase = 'Installation'
+
+		## Handle Zero-Config MSI Installations
+		If ($useDefaultMsi) {
+			[hashtable]$ExecuteDefaultMSISplat =  @{ Action = 'Install'; Path = $defaultMsiFile }; If ($defaultMstFile) { $ExecuteDefaultMSISplat.Add('Transform', $defaultMstFile) }
+			Execute-MSI @ExecuteDefaultMSISplat; If ($defaultMspFiles) { $defaultMspFiles | ForEach-Object { Execute-MSI -Action 'Patch' -Path $_ } }
+		}
+
+		## <Perform Installation tasks here>
+		
+		Execute-Process -Path “IE11-Setup-Branding.exe” -Parameters ‘/q’
+		Set-RegistryKey -Key "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "DisableTaskMgr" -Value 1 -Type DWORD
+		Set-RegistryKey -Key "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Ext" -Name "IgnoreFrameApprovalCheck" -Value 1 -Type DWORD
+		Set-RegistryKey -Key "HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Policies\Ext" -Name "IgnoreFrameApprovalCheck" -Value 1 -Type DWORD
+
+		Dism /online /Enable-Feature /all /FeatureName:Client-EmbeddedShellLauncher /quiet /norestart
+
+		# Check if shell launcher license is enabled
+		function Test-ShellLauncherLicenseEnabled
+		{
+		    [string]$source = @"
+		using System;
+		using System.Runtime.InteropServices;
+
+		static class CheckShellLauncherLicense
+		{
+		    const int S_OK = 0;
+
+		    public static bool IsShellLauncherLicenseEnabled()
+		    {
+		        int enabled = 0;
+
+		        if (NativeMethods.SLGetWindowsInformationDWORD("EmbeddedFeature-ShellLauncher-Enabled", out enabled) != S_OK) {
+		            enabled = 0;
+		        }
+
+		        return (enabled != 0);
+		    }
+
+		    static class NativeMethods
+		    {
+		        [DllImport("Slc.dll")]
+		        internal static extern int SLGetWindowsInformationDWORD([MarshalAs(UnmanagedType.LPWStr)]string valueName, out int value);
+		    }
+
+		}
+"@
+		    $type = Add-Type -TypeDefinition $source -PassThru
+
+		    return $type[0]::IsShellLauncherLicenseEnabled()
+		}
+
+		[bool]$result = $false
+
+		$result = Test-ShellLauncherLicenseEnabled
+		"`nShell Launcher license enabled is set to " + $result
+		if (-not($result))
+		{
+		    "`nThis device doesn't have required license to use Shell Launcher"
+		    exit
+		}
+
+		$COMPUTER = "localhost"
+		$NAMESPACE = "root\standardcimv2\embedded"
+
+		# Create a handle to the class instance so we can call the static methods.
+		try {
+		    $ShellLauncherClass = [wmiclass]"\\$COMPUTER\${NAMESPACE}:WESL_UserSetting"
+		    } catch [Exception] {
+		    write-output $_.Exception.Message;
+		    write-output "Make sure Shell Launcher feature is enabled"
+		    exit
+		    }
+
+
+		# This well-known security identifier (SID) corresponds to the BUILTIN\Administrators group.
+
+		$Admins_SID = "S-1-5-32-544"
+
+		# Create a function to retrieve the SID for a user account on a machine.
+
+		function Get-UsernameSID($AccountName) {
+
+		    $NTUserObject = New-Object System.Security.Principal.NTAccount($AccountName)
+		    $NTUserSID = $NTUserObject.Translate([System.Security.Principal.SecurityIdentifier])
+
+		    return $NTUserSID.Value
+
+		}
+
+		# Get the SID for a user account named "Cashier". Rename "Cashier" to an existing account on your system to test this script.
+
+		# $Cashier_SID = Get-UsernameSID($PlainTextAccount)
+
+		# Define actions to take when the shell program exits.
+
+		$restart_shell = 0
+		# $restart_device = 1
+		# $shutdown_device = 2
+
+		# Examples. You can change these examples to use the program that you want to use as the shell.
+
+
+		# This example sets the command prompt as the default shell, and restarts the device if the command prompt is closed.
+
+		    $ShellLauncherClass.SetDefaultShell("c:\program files\internet explorer\iexplore.exe -k https://www.unisainc.com/pbweb/", $restart_shell)
+
+		# Display the default shell to verify that it was added correctly.
+
+		    # $DefaultShellObject = $ShellLauncherClass.GetDefaultShell()
+
+		    # "`nDefault Shell is set to " + $DefaultShellObject.Shell + " and the default action is set to " + $DefaultShellObject.defaultaction
+
+		# Set Internet Explorer as the shell for "Cashier", and restart the machine if Internet Explorer is closed.
+
+		    #$ShellLauncherClass.SetCustomShell($Cashier_SID, "c:\program files\internet explorer\iexplore.exe -k https://www.unisainc.com/pbweb/", ($null), ($null), $restart_shell)
+
+		# Set Explorer as the shell for administrators.
+
+		    $ShellLauncherClass.SetCustomShell($Admins_SID, "explorer.exe")
+
+		# View all the custom shells defined.
+
+		    # "`nCurrent settings for custom shells:"
+		    # Get-WmiObject -namespace $NAMESPACE -computer $COMPUTER -class WESL_UserSetting | Select Sid, Shell, DefaultAction
+
+		# Enable Shell Launcher
+
+		     $ShellLauncherClass.SetEnabled($TRUE)
+
+
+		# Remove the new custom shells.
+
+		    # $ShellLauncherClass.RemoveCustomShell($Admins_SID)
+
+		    # $ShellLauncherClass.RemoveCustomShell($Cashier_SID)
+
+		# Disable Shell Launcher
+
+		    # $ShellLauncherClass.SetEnabled($FALSE)
+
+		    # $IsShellLauncherEnabled = $ShellLauncherClass.IsEnabled()
+
+		    # "`nEnabled is set to " + $IsShellLauncherEnabled.Enabled
+
+Set-RegistryKey -Key "HKEY_LOCAL_MACHINE\SOFTWARE\MSUDenver" -Name "PerkinsSet" -Value 'installed' -Type String
+
+		##*===============================================
+		##* POST-INSTALLATION
+		##*===============================================
+		[string]$installPhase = 'Post-Installation'
+
+		## <Perform Post-Installation tasks here>
+
+		## Display a message at the end of the install
+		If (-not $useDefaultMsi) {
+
+		}
+	}
+	ElseIf ($deploymentType -ieq 'Uninstall')
+	{
+		##*===============================================
+		##* PRE-UNINSTALLATION
+		##*===============================================
+		[string]$installPhase = 'Pre-Uninstallation'
+
+		## Show Welcome Message, close Internet Explorer with a 60 second countdown before automatically closing
+		Show-InstallationWelcome -CloseApps 'iexplore' -CloseAppsCountdown 60
+
+		## Show Progress Message (with the default message)
+		Show-InstallationProgress
+
+		## <Perform Pre-Uninstallation tasks here>
+
+
+		##*===============================================
+		##* UNINSTALLATION
+		##*===============================================
+		[string]$installPhase = 'Uninstallation'
+
+		## Handle Zero-Config MSI Uninstallations
+		If ($useDefaultMsi) {
+			[hashtable]$ExecuteDefaultMSISplat =  @{ Action = 'Uninstall'; Path = $defaultMsiFile }; If ($defaultMstFile) { $ExecuteDefaultMSISplat.Add('Transform', $defaultMstFile) }
+			Execute-MSI @ExecuteDefaultMSISplat
+		}
+
+		# <Perform Uninstallation tasks here>
+
+
+		##*===============================================
+		##* POST-UNINSTALLATION
+		##*===============================================
+		[string]$installPhase = 'Post-Uninstallation'
+
+		## <Perform Post-Uninstallation tasks here>
+
+
+	}
+
+	##*===============================================
+	##* END SCRIPT BODY
+	##*===============================================
+
+	## Call the Exit-Script function to perform final cleanup operations
+	Exit-Script -ExitCode $mainExitCode
+}
+Catch {
+	[int32]$mainExitCode = 60001
+	[string]$mainErrorMessage = "$(Resolve-Error)"
+	Write-Log -Message $mainErrorMessage -Severity 3 -Source $deployAppScriptFriendlyName
+	Show-DialogBox -Text $mainErrorMessage -Icon 'Stop'
+	Exit-Script -ExitCode $mainExitCode
+}
